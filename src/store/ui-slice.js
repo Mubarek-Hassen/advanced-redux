42dd3d0066a62dcb7d5cfefa5d77e8fae2d89c98
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-11-30 22:11:42 -0500

    ui action dispatched

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -3,11 +3,11 @@ import {createSlice} from '@reduxjs/toolkit'
 const uiSlice = createSlice({
   name: 'ui',
   initialState: {cartIsVisible: false},
-  reducer: {
+  reducers: {
     toggle(state){
       state.cartIsVisible = !state.cartIsVisible
     }
   }
 })
-
+export const uiActions = uiSlice.actions
 export default uiSlice
